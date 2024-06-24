50b68f95467e04fa72c5d59ed9f66617ef323d14
pavani-1510 <pavanikpm05@gmail.com>
2024-06-24 14:37:07 +0530

    first commit

--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db import models
+from django.contrib.auth.models import User
+
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.sender.username} - {self.timestamp}'
+        return f'{self.sender} -> {self.receiver}: {self.content}'
 
 # models.py
 
@@ -20,3 +24,4 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
--- chat/urls.py
+++ chat/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('users/', user_list, name='user_list'),
     path('chat/<int:user_id>/', views.chat_with_user, name='chat_with_user'),
     path('delete-all-messages/', views.delete_all_messages, name='delete_all_messages'),
-
+    path('fetch-messages/<int:receiver_id>/', views.fetch_messages, name='fetch_messages'),
     path('send-message/<int:user_id>/', views.send_message, name='send_message'),
 
 ]
